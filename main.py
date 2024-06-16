e1d58141855c6a47c7804f5fbfd4a423352d90a9
Mehmet Ali YILMAZ <mali.yilmaz@rakort.com>
2024-06-17 01:25:02 +0300

    added middleware

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-from fastapi import FastAPI
 from routes.posts import router as posts_router
 from routes.results import router as results_router
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
+from fastapi import FastAPI, middleware
+middleware = [
+    middleware.Middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    ),
+    middleware.Middleware(
+        TrustedHostMiddleware,
+        allowed_hosts=["*"],
+    )
+]
 
-app = FastAPI()
+app = FastAPI(middleware=middleware)
 
 app.include_router(posts_router, prefix="/posts")
 app.include_router(results_router, prefix="/results")
